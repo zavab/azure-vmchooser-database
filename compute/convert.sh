6a9e821bca04a9501377fb10f0e0f7ad318f2763
Karim Vaes <karim@kvaes.be>
2018-03-27 12:38:31 +0200

    changed convert to include opinionated data include

--- compute/convert.sh
+++ compute/convert.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 nodejs node_modules/csvtojson/bin/csvtojson newpricing.csv > newpricing.json
+
+echo 'var ROW_DATA = "' > data.js
+cat newpricing.json >> data.js
+echo '";' >> data.js
+echo 'function createRowData() { return ROW_DATA; }' >> data.js
