63ac6d2ab226843710634d14604ee3d6d1843efa
Karim Vaes <karim@kvaes.be>
2018-06-06 22:05:16 +0200

    Added constrained capability

--- compute/parse.js
+++ compute/parse.js
@@ -9,7 +9,7 @@ var pricing = {
     "ri3y": './apipricing-base-3y.json'
 };
 
-console.log("name,type,contract,tier,cores,pcores,mem,region,price,ACU,SSD,MaxNics,Bandwidth,MaxDataDiskCount,MaxDataDiskSizeGB,MaxDataDiskIops,MaxDataDiskThroughputMBs,MaxVmIops,MaxVmThroughputMBs,ResourceDiskSizeInMB,TempDiskSizeInGB,TempDiskIops,TempDiskReadMBs,TempDiskWriteMBs,SAPS2T,SAPS3T,SAPHANA,Hyperthreaded,OfferName,_id,price_USD,price_EUR,price_GBP,price_AUD,price_JPY,price_CAD,price_DKK,price_CHF,price_SEK,price_IDR,price_INR,burstable,isolated,os");
+console.log("name,type,contract,tier,cores,pcores,mem,region,price,ACU,SSD,MaxNics,Bandwidth,MaxDataDiskCount,MaxDataDiskSizeGB,MaxDataDiskIops,MaxDataDiskThroughputMBs,MaxVmIops,MaxVmThroughputMBs,ResourceDiskSizeInMB,TempDiskSizeInGB,TempDiskIops,TempDiskReadMBs,TempDiskWriteMBs,SAPS2T,SAPS3T,SAPHANA,Hyperthreaded,OfferName,_id,price_USD,price_EUR,price_GBP,price_AUD,price_JPY,price_CAD,price_DKK,price_CHF,price_SEK,price_IDR,price_INR,burstable,isolated,constrained,os");
 
 for (var pricesheet in pricing) {
     var jsonfile = require(pricing[pricesheet]);
@@ -73,6 +73,11 @@ for (var pricesheet in pricing) {
                     var isolated = "No";
                     if (picked.Isolated !== undefined) {
                         var isolated = "Yes";
+                    }
+                    // Constrained check
+                    var constrained = "No";
+                    if (picked.Constrained !== undefined) {
+                        var constrained = "Yes";
                     }
                     // SAP HANA check
                     var SAPHANA = "No";
@@ -138,6 +143,7 @@ for (var pricesheet in pricing) {
                             priceINR + "," +
                             burstable + "," +
                             isolated + "," +
+                            constrained + "," +
                             os);
                     } else {
                         console.log(name + "@found@standard@" + pricesheet);
@@ -169,6 +175,11 @@ for (var pricesheet in pricing) {
                         var isolated = "No";
                         if (picked.Isolated !== undefined) {
                             var isolated = "Yes";
+                        }
+                        // Constrained check
+                        var constrained = "No";
+                        if (picked.Constrained !== undefined) {
+                            var constrained = "Yes";
                         }
                         // SAP HANA check
                         var SAPHANA = "No";
@@ -233,6 +244,7 @@ for (var pricesheet in pricing) {
                                 priceINR + "," +
                                 burstable + "," +
                                 isolated + "," +
+                                constrained + "," +
                                 os);
                         } else {
                             console.log(name + "@found@premium@" + pricesheet);
