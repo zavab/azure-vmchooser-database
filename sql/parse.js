e7f23b52515ddbf8ee7dff2628d8ea877a2e0c23
Karim Vaes <karim@kvaes.be>
2018-08-20 12:43:12 +0200

    Added reference data on storage and memory

--- sql/parse.js
+++ sql/parse.js
@@ -3,7 +3,7 @@ var currency = require('./currency.json');
 var offers = jsonfile.offers;
 
 console.log(
-    'offer,' +
+    'name,' +
     'region,' +
     'type,' +
     'tier,' +
@@ -12,6 +12,10 @@ console.log(
     'ahub,' +
     'compute,' +
     'cores,' +
+    'memory,' +
+    'maxstorage,' +
+    'maxiops,' +
+    'maxthroughput,' +
     'storage,' +
     'priceUSD,' +
     'priceEUR,' +
@@ -36,6 +40,10 @@ for (var offer in offers) {
     var cores = 0;
     var contract = 'payg';
     var ahub = 'no';
+    var memory = 0;
+    var maxstorage = 999999; //TB
+    var maxiops = 0;
+    var maxthroughput = 0;
 
     if (type !== 'vcore' && type !== 'dtu' && type !== 'storage') {
         console.log('Unknown type!');
@@ -69,18 +77,112 @@ for (var offer in offers) {
         }
     }
 
+    // Calculate Memory Sizes
+    if (offername[4] === 'gen4') {
+        memory = cores * 7;
+    }
+    if (offername[4] === 'gen5') {
+        memory = cores * 5.5;
+    }
+
+    // Max Storage
+    // - Single Database
+    if (purchasemodel === 'single') {
+        maxiops = cores * 250;
+        if (maxiops > 7000) { maxiops = 7000; }
+        maxthroughput = (maxiops * 64) / 1024;
+        if (offername[4] === 'gen4') {
+            if (cores < 4) { maxstorage = 1; }
+            if (cores < 8) { maxstorage = 1.5; }
+            if (cores === 8) { maxstorage = 3; }
+            if (cores > 8) { maxstorage = 4; }
+        }
+        if (offername[4] === 'gen5') {
+            if (cores < 8) { maxstorage = 1; }
+            if (cores === 8) { maxstorage = 1.5; }
+            if (cores < 24) { maxstorage = 3; }
+            if (cores >= 24) { maxstorage = 4; }
+        }
+    }
+    // - Single Database
+    if (purchasemodel === 'elastic') {
+        if (offername[4] === 'gen4' & offername[2] === 'general') {
+            maxiops = cores * 500;
+            if (maxiops > 7000) { maxiops = 7000; }
+            maxthroughput = (maxiops * 64) / 1024;
+            if (cores === 1) { maxstorage = 0.5; }
+            if (cores === 2) { maxstorage = 0.75; }
+            if (cores === 4) { maxstorage = 1.5; }
+            if (cores === 8) { maxstorage = 2; }
+            if (cores === 16) { maxstorage = 3.5; }
+            if (cores === 24) { maxstorage = 3; }
+        }
+        if (offername[4] === 'gen5' & offername[2] === 'general') {
+            maxiops = cores * 250;
+            if (maxiops > 7000) { maxiops = 7000; }
+            maxthroughput = (maxiops * 64) / 1024;
+            if (cores === 2) { maxstorage = 0.5; }
+            if (cores === 4) { maxstorage = 0.75; }
+            if (cores === 8) { maxstorage = 1.5; }
+            if (cores === 16) { maxstorage = 2; }
+            if (cores === 24) { maxstorage = 3; }
+            if (cores === 32) { maxstorage = 4; }	
+        }
+        if (offername[4] === 'gen4' & offername[2] === 'business') {
+            maxiops = cores * 5000;
+            if (maxiops > 80000) { maxiops = 80000; }
+            maxthroughput = cores * 125;
+            maxstorage = cores;
+        }
+        if (offername[4] === 'gen5' & offername[2] === 'business') {
+            maxiops = cores * 2500;
+            maxthroughput = cores * (125 / 2);
+            maxstorage = cores / 2;
+        }
+    }
+    // - Managed Instances
+    if (offername[2] === 'general' && purchasemodel === 'managed') {
+        maxstorage = 8;
+        maxiops = 5000 * 8;
+        maxthroughput = 125 * 8;
+    }
+    if (offername[2] === 'business' && purchasemodel === 'managed') {
+        if (offername[4] === 'gen4') {
+            maxstorage = 1;
+            maxiops = 5000 * 1;
+            maxthroughput = 125 * 1;
+        }
+        if (offername[4] === 'gen5') {
+            if (cores < 24) {
+                maxstorage = 1;
+                maxiops = 5000 * 1;
+                maxthroughput = 125 * 1;
+            }
+            if (cores < 32) {
+                maxstorage = 2;
+                maxiops = 5000 * 2;
+                maxthroughput = 125 * 2;
+            }
+            if (cores < 81) {
+                maxstorage = 4;
+                maxiops = 5000 * 4;
+                maxthroughput = 125 * 4;
+            }
+        }
+    }
+
+    // Storage Related
     if (offername[2] === "operations") {
         tier = offername[2];
     }
 
+    // Contract Options
     if (offername[8] === 'hybrid' || offername[11] === 'hybrid') {
         ahub = 'yes';
     }
-
     if (offername[10] === 'reserved' && offername[8] === 'one') {
         contract = 'ri1y';
     }
-
     if (offername[10] === 'reserved' && offername[8] === 'three') {
         contract = 'ri3y';
     }
@@ -110,6 +212,10 @@ for (var offer in offers) {
                 ahub + ',' +
                 compute + ',' +
                 cores + ',' +
+                memory + ',' +
+                maxstorage + ',' +
+                maxiops + ',' +
+                maxthroughput + ',' +
                 storage + ',' +
                 priceUSD + ',' +
                 priceEUR + ',' +
