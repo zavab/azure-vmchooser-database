e981994cb44c99ad126c377048f9d24c398f4f93
Karim Vaes <karim@kvaes.be>
2019-05-15 21:19:38 +0200

    Update for new backup result format

--- backup/parse.js
+++ backup/parse.js
@@ -1,7 +1,6 @@
 var jsonfile = require('./backuppricing.json');
 var currency = require('./currency.json');
 var offers = jsonfile.offers;
-var graduatedOffers = jsonfile.graduatedOffers;
 
 console.log(
     'name,' +
@@ -27,72 +26,37 @@ console.log(
     '_id'
 );
 
-// Backup service cost
-var dimension = 'backup-instance';
-for (var region in offers['backup'].prices) {
-    var price = offers['backup'].prices[region];
-    var priceUSD = price.value;
-    var priceEUR = priceUSD * currency.eur.conversion;
-    var priceGBP = priceUSD * currency.gbp.conversion;
-    var priceAUD = priceUSD * currency.aud.conversion;
-    var priceJPY = priceUSD * currency.jpy.conversion;
-    var priceCAD = priceUSD * currency.cad.conversion;
-    var priceDKK = priceUSD * currency.dkk.conversion;
-    var priceCHF = priceUSD * currency.chf.conversion;
-    var priceSEK = priceUSD * currency.sek.conversion;
-    var priceIDR = priceUSD * currency.idr.conversion;
-    var priceINR = priceUSD * currency.inr.conversion;
-    var priceRUB = priceUSD * currency.rub.conversion;
-    id = dimension + '-' + region;
-    console.log(
-        id + ',' +
-        region + ',' +
-        'backup' + ',' +
-        dimension + ',' +
-        offers['backup'].incrementalSizeGB + ',' +
-        offers['backup'].discountedSizeGB + ',' +
-        offers['backup'].discountedRate + ',' +
-        priceUSD + ',' +
-        priceUSD + ',' +
-        priceEUR + ',' +
-        priceGBP + ',' +
-        priceAUD + ',' +
-        priceJPY + ',' +
-        priceCAD + ',' +
-        priceDKK + ',' +
-        priceCHF + ',' +
-        priceSEK + ',' +
-        priceIDR + ',' +
-        priceINR + ',' +
-        priceRUB + ',' +
-        id
-    );
-}
-
-// Backup Storage cost
-for (dimension in graduatedOffers) {
-    for (region in graduatedOffers[dimension]) {
-        priceUSD = graduatedOffers[dimension][region].prices[0].price.value;
-        priceEUR = priceUSD * currency.eur.conversion;
-        priceGBP = priceUSD * currency.gbp.conversion;
-        priceAUD = priceUSD * currency.aud.conversion;
-        priceJPY = priceUSD * currency.jpy.conversion;
-        priceCAD = priceUSD * currency.cad.conversion;
-        priceDKK = priceUSD * currency.dkk.conversion;
-        priceCHF = priceUSD * currency.chf.conversion;
-        priceSEK = priceUSD * currency.sek.conversion;
-        priceIDR = priceUSD * currency.idr.conversion;
-        priceINR = priceUSD * currency.inr.conversion;
-        priceRUB = priceUSD * currency.rub.conversion;
+// Backup cost
+for (var offer in offers) {
+    console.log(offer);
+    if (offer === "backup") {
+        dimension = "backup-instance";
+    } else {
+        dimension = offer;
+    }
+    for (var region in offers[offer].prices) {
+        var price = offers[offer].prices[region];
+        var priceUSD = price.value;
+        var priceEUR = priceUSD * currency.eur.conversion;
+        var priceGBP = priceUSD * currency.gbp.conversion;
+        var priceAUD = priceUSD * currency.aud.conversion;
+        var priceJPY = priceUSD * currency.jpy.conversion;
+        var priceCAD = priceUSD * currency.cad.conversion;
+        var priceDKK = priceUSD * currency.dkk.conversion;
+        var priceCHF = priceUSD * currency.chf.conversion;
+        var priceSEK = priceUSD * currency.sek.conversion;
+        var priceIDR = priceUSD * currency.idr.conversion;
+        var priceINR = priceUSD * currency.inr.conversion;
+        var priceRUB = priceUSD * currency.rub.conversion;
         id = dimension + '-' + region;
         console.log(
             id + ',' +
             region + ',' +
             'backup' + ',' +
             dimension + ',' +
-            '' + ',' +
-            '' + ',' +
-            '' + ',' +
+            offers[offer].incrementalSizeGB + ',' +
+            offers[offer].discountedSizeGB + ',' +
+            offers[offer].discountedRate + ',' +
             priceUSD + ',' +
             priceUSD + ',' +
             priceEUR + ',' +
